3ec368cabdfc3a432b3be22100feaac5c1aed07f
Blake Griffith <blake.a.griffith@gmail.com>
2015-05-05 17:32:21 -0500

    Add default chunk selection.

--- skimage/util/process.py
+++ skimage/util/process.py
@@ -1,9 +1,33 @@
+from multiprocessing import cpu_count
+
 import dask.array as da
 
 __all__ = ['process_chunks']
 
 
-def process_chunks(function, array, chunks, depth=None, mode=None):
+def _get_chunks(shape, ncpu):
+    """
+    Split the array into equal sized chunks based on the number of
+    available processors. The last chunk in each dimension absorbs the
+    remainder array elements if the number of cpus does not divide evenly into
+    the number of array elements.
+    """
+    chunks = []
+
+    for i in shape:
+        regular_chunk = i // ncpu
+        remainder_chunk = regular_chunk + (i % ncpu)
+
+        if regular_chunk == 0:
+            chunk_lens = (remainder_chunk,)
+        else:
+            chunk_lens = (regular_chunk,) * (ncpu - 1) + (remainder_chunk,)
+
+        chunks.append(chunk_lens)
+    return tuple(chunks)
+
+
+def process_chunks(function, array, chunks=None, depth=0, mode=None):
     """Map a function in parallel across an array.
 
     Split an array into possibly overlapping chunks of a given depth and
@@ -19,7 +43,8 @@ def process_chunks(function, array, chunks, depth=None, mode=None):
     chunks : int, tuple, or tuple of tuples
         One tuple of length array.ndim or a list of tuples of length ndim.
         Where each subtuple adds to the size of the array in the corresponding
-        dimension.
+        dimension. If None, the array is broken up into chunks based on the
+        number of available cpus.
     depth : int
         integer equal to the depth of the internal external padding
     mode : 'reflect', 'periodic', 'wrap', 'nearest'
@@ -31,6 +56,11 @@ def process_chunks(function, array, chunks, depth=None, mode=None):
     a chunk.
 
     """
+    if chunks == None:
+        shape = array.shape
+        ncpu = cpu_count()
+        chunks = _get_chunks(shape, ncpu)
+
     if mode == 'wrap':
         mode = 'periodic'
 
